b3cf60488d5b7287d105e3ae815ced797d528d23
Luísa Cristina Ferreira <luisac.paim@gmail.com>
2022-10-31 11:27:09 -0300

    alterando background-img

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 let id = 0;
-let height = 0; // no máximo 8 captar a altura de cada div, somar à variavel a cada adição e condicionar a adição até a soma igualar a altura total de tasks
-// let altura = document.querySelector('.taskList').style.max-height;
-// console.log(altura)      LIMITAR nº de entradas a altura ;
+let height = 0;
 
 const data = {
     colectTasks: () => JSON.parse(localStorage.getItem('tasks-list')),
     editTasks: (taskList) => localStorage.setItem('tasks-list', JSON.stringify(taskList))
 }
 
-/* const tarefa recebe e cria div para cada nova tarefa c/ prazo adicionada */
 const task = (id, task, deadline) => ` <div class="taskDescription">          
                                             <input class="taskCheck" type="checkbox" onchange="checkTask(${id})" />
                                             <p class="taskName" id="${id}">${task}</p>
                                             <img class="taskDelete" onclick="removeTask(${id})" src="./img/trash_icon.svg" alt="">                            
                                         </div>` 
-                                            // <p>${deadline}</p>
-/* função que varia id, limita a quant de criação de tarefa a 8, 
-coleta o valor do input newTask e 
-chama a const/função tarefa passando o valor de id, newTask e o prazo*/
 
 const checkTask = (id) => {
     const strike = document.getElementById(`strike${id}`);
@@ -45,14 +38,10 @@ const validateTask = (newTask) => {
 
     if (taskList) {
         taskList.map(task => {
-            /*if (newTask === '') {   VALIDAR SE O CAMPO NÃO ESTÁ VAZIO
-                alert('Digite a descrição de uma tarefa.')
-            }*/ if (task === newTask) {
+            if (task === newTask) {
                 existingTask = true;
                 alert('Tareja já existente. Adicione nova!');
-            } /*if (newTask === '') {     //CONFERIR
-                alert('Digite a descrição de uma tarefa.');
-            }   NÂO deu certo*/ 
+            }
         });
     }
 
@@ -85,11 +74,9 @@ function addTask() {
     }
 }
 
-const removeTask = (id) => {    //deletar tarefa o clicar na imagem
-    // "pega" a task em questão
+const removeTask = (id) => {
     const deletedTask = document.getElementById(id).innerHTML;
     const tasksList = data.colectTasks();
-    //cria nova lista (filtrando/removendo) sem a task deletada
     const newTaskList = tasksList.filter(task => task != deletedTask);
     data.editTasks(newTaskList);
     document.querySelector('#tasks').innerHTML = '';
